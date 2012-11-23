151e32f4953b9348af56dae0e4a98c3eb995f554
Milton <mbaltazar.v7@gmail.com>
2012-11-23 00:49:13 -0500

    arquitectura del proyecto

---
+++ BENEDICTOXVI/src/benedictoxvi/Entidades/BasePersona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package benedictoxvi.Entidades;
+
+/**
+ *
+ * @author milton
+ */
+public class BasePersona {
+    private String nombre;
+    private String apellidoPaterno;
+    private String sexo;
+    private String nroDocumento;
+    private String tipoDocumento;
+    private String estadoCivil;
+    private boolean estado;
+}
---
+++ BENEDICTOXVI/src/benedictoxvi/Entidades/Curso.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package benedictoxvi.Entidades;
+
+/**
+ *
+ * @author milton
+ */
+public class Curso {
+    private String CodigoCurso;
+    private String NombreCurso;
+            
+}
