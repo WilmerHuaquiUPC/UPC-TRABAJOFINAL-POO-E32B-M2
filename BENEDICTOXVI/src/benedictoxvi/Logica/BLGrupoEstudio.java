90a0212cbc18b8c9c917b3a90006f6e33df9c145
Milton <mbaltazar.v7@gmail.com>
2012-11-26 00:21:56 -0500

    clases para la construccion del caso de usuario: grupo de estudios

---
+++ BENEDICTOXVI/src/benedictoxvi/Logica/BLGrupoEstudio.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package benedictoxvi.Logica;
+
+import benedictoxvi.Entidades.GrupoEstudio;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author milton
+ */
+public class BLGrupoEstudio {
+   
+public ArrayList<GrupoEstudio> BuscarGrupoEstudio(GrupoEstudio pGrupoEstudio)
+{
+    return null;
+}
+
+public boolean GuardarGrupoEstudio(GrupoEstudio pGrupoEstudio)
+{
+    return false;
+}
+
+public boolean ModificarGrupoEstudio(GrupoEstudio pGrupoEstudio)
+{
+    return false;
+}
+
+public boolean EliminarGrupoEstudio(GrupoEstudio pGrupoEstudio)
+{
+    return false;
+}
+
+}
